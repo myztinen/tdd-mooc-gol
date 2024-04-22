a03e25686ea1a1fd84ccff0ec34341d8093cdada
myztinen <myztinen@gmail.com>
2024-04-22 20:03:22 +0300

    Reading file works

--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,9 +1,15 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { sum } from "../src/example.mjs";
+import { main } from "../src/main.mjs";
 
-describe("Example test fixture", () => {
-  test("Example test", () => {
-    expect(sum(1, 2)).to.equal(3);
+
+describe("First test", () => {
+  test("Can read file", () => {
+    let fileContents = main("./resourses/block.rle");
+    expect(fileContents.comments).to.equal("#N Block\n#C An extremely common 4-cell still life.\n#C www.conwaylife.com/wiki/index.php?title=Block\n");
+    expect(fileContents.header).to.equal("x = 2, y = 2, rule = B3/S23");
+    expect(fileContents.encodedPattern).to.equal("2o$2o!");
   });
+
+
 });
