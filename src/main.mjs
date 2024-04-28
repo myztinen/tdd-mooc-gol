cd585ada0d683f9af4ce1baee1a3ed077e060fcb
myztinen <myztinen@gmail.com>
2024-04-28 19:02:29 +0300

    Encoding works

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,6 @@
 const fs = require('fs');
-import { parseRLE, encodedDataToFile} from "./parsing.mjs";
+import { parseRLE, encodedDataToFile, decodeRLEPattern,
+  encodeRLEPattern} from "./parsing.mjs";
 
 
 export function main(filePath, iterations) {
@@ -11,6 +12,8 @@ export function main(filePath, iterations) {
   }
 
   let parsedContents = parseRLE(fileContents);
+  let decodedPattern = decodeRLEPattern(parsedContents.encodedPattern);
+  let encodedDataPattern = encodeRLEPattern(decodedPattern);
   let processedContents = encodedDataToFile(parsedContents);
   return processedContents;
 
